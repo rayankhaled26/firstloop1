8f2d56855d734811fc4e99e8a96d8f64ed0a4800
rayankhaled26 <rk217230@gmail.com>
2022-12-31 11:38:05 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#firstloops 1
+# Display numbers from a list using a loop
+
+numbers = [12, 75, 150, 180, 145, 525, 50]
+for value in numbers:
+    if value % 5 == 0:
+        if value <= 150:
+            print(value)
+            continue
+        if value > 500:
+            break
+
+
+
+
+
+
+
+
